e58d6611f06b2acfc28e5f11fbb52d287317ea81
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-12-21 19:57:01 -0500

    added imports for redux in app file

--- src/store.js
+++ src/store.js
@@ -1,6 +1,10 @@
 import { createStore } from "redux";
 import rotateReducer from "reducers/rotateReducer";
 
+//Imports for redux
+import { connect } from "react-redux";
+
+
 function configureStore( state = { rotating: true }) {
   return createStore(rotateReducer, state);
 }
