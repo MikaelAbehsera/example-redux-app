7be6580089b1e68f98f80d45672662737787bc7d
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-12-22 15:44:07 -0500

    added clickable img + rotating code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 //  Imports for redux
 import { connect } from "react-redux";
@@ -8,13 +8,24 @@ import { connect } from "react-redux";
 import { startAction } from "./actions/startAction";
 import { stopAction } from "./actions/stopAction";
 
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <img
+            src={logo}
+            className={
+              "App-logo" + (this.props.rotating ? "" : " App-logo-paused")
+            }
+            alt="logo"
+            onClick={
+              this.props.rotating
+                ? this.props.stopAction
+                : this.props.startAction
+            }
+          />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -33,12 +44,15 @@ class App extends Component {
 }
 
 const mapStateToProps = state => ({
-  ...state
+  ...state,
 });
 
 const mapDispatchToProps = dispatch => ({
   startAction: () => dispatch(startAction),
-  stopAction: () => dispatch(stopAction)
+  stopAction: () => dispatch(stopAction),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App);
