d7a22ed466b71f5b0b64e0522513e61ea82cee08
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-12-22 15:37:39 -0500

    changed app.js export

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -37,7 +37,8 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  
-})
+  startAction: () => dispatch(startAction),
+  stopAction: () => dispatch(stopAction)
+});
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
