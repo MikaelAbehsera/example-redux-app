ad2f7926dfcdbf9c49025dd3b4f552c20ef110e0
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-12-22 16:29:48 -0500

    changed import for store, added action fole

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // new imports start
 import { Provider } from "react-redux";
-import configureStore from "store";
+import configureStore from "./store";
 // new imports stop
 import './index.css';
 import App from './App';
